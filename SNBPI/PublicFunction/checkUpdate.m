cd0cc436dd47f98210a80092033f6c71e3e1cd66
ZhangTianhao1993 <thzhang@ihep.ac.cn>
2026-04-09 20:49:24 +0800

    add some installinstructions for checkUpdate

--- SNBPI/PublicFunction/checkUpdate.m
+++ SNBPI/PublicFunction/checkUpdate.m
@@ -4,36 +4,32 @@ function checkUpdate(showNoUpdateMsg)
 if nargin < 1
     showNoUpdateMsg = false;
 end
-
 url = 'https://raw.githubusercontent.com/ZhangTianhao1993/Spatial-Normalization-of-Brain-PET-Images/main/SNBPI/version.json';
-
 try
     raw = webread(url, weboptions('Timeout', 5));
     remoteInfo = jsondecode(raw);
     remoteVer  = remoteInfo.version;
     localVer   = getLocalVersion();
-
     if compareVersion(remoteVer, localVer) > 0
         msg = sprintf(['New version v%s is available (current: v%s)\n\n'...
-                       'What''s new:\n%s\n\n'...
-                       'Would you like to download it?'], ...
-                       remoteVer, localVer, remoteInfo.description);
-
+            'What''s new:\n%s\n\n'...
+            'Would you like to download it?'], ...
+            remoteVer, localVer, remoteInfo.description);
         if remoteInfo.mandatory
             uiwait(msgbox(sprintf('This is a mandatory update. Please download before use.\n\n%s', msg), ...
                 'Mandatory Update', 'warn'));
             web(remoteInfo.download_url, '-browser');
+            showInstallInstructions();
         else
             choice = questdlg(msg, 'Update Available', 'Download', 'Later', 'Download');
             if strcmp(choice, 'Download')
                 web(remoteInfo.download_url, '-browser');
+                showInstallInstructions();
             end
         end
-
     elseif showNoUpdateMsg
         msgbox(sprintf('You are using the latest version v%s.', localVer), 'Check for Updates');
     end
-
 catch
     if showNoUpdateMsg
         warndlg('Unable to connect to the update server. Please check your network connection.', 'Update Check Failed');
@@ -41,6 +37,27 @@ catch
 end
 end
 
+function showInstallInstructions()
+% Show manual installation instructions after download
+mainApp = which('SNBPI.mlapp');
+if isempty(mainApp)
+    oldPath = '(SNBPI installation folder not found, please locate it manually)';
+else
+    oldPath = fileparts(mainApp);
+end
+instructions = sprintf([...
+    'The new version is being downloaded in your browser.\n\n' ...
+    'Please follow these steps to install:\n\n' ...
+    '1. Extract the downloaded ZIP file to a location of your choice.\n' ...
+    '2. Remove the old version from MATLAB:\n' ...
+    '      - Go to HOME -> Set Path, remove the old SNBPI folder, and click Save.\n' ...
+    '      - Or delete the old folder directly:\n        %s\n' ...
+    '3. Add the new SNBPI folder to the MATLAB path\n' ...
+    '   (HOME -> Set Path -> Add with Subfolders -> Save).\n' ...
+    '4. Restart SNBPI.\n\n' ...
+    'Note: Please close SNBPI before deleting the old version.'], oldPath);
+uiwait(msgbox(instructions, 'Manual Installation Required', 'help'));
+end
 
 function result = compareVersion(v1, v2)
 % Compare version strings. Returns 1 if v1>v2, 0 if equal, -1 if v1<v2
